3b871ed16a83592bfc0e5554b86b6dc52f9f728f
Antoine Delia <antoine.delia@gmail.com>
2025-06-01 16:28:17 +0200

    feat: start adding the frontend infra

---
+++ iac-cdk/frontend/frontend_stack.py
@@ -0,0 +1,53 @@
+from aws_cdk import CfnOutput, RemovalPolicy, Stack
+from aws_cdk import (
+    aws_cloudfront as cloudfront,
+)
+from aws_cdk import (
+    aws_cloudfront_origins as origins,
+)
+from aws_cdk import (
+    aws_s3 as s3,
+)
+from aws_cdk import (
+    aws_s3_deployment as s3deploy,
+)
+from constructs import Construct
+
+
+class FrontendStack(Stack):
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        # S3 Bucket (private)
+        bucket = s3.Bucket(
+            self,
+            "FrontendBucket",
+            removal_policy=RemovalPolicy.DESTROY,
+            auto_delete_objects=True,
+            website_index_document="index.html",
+            public_read_access=False,
+            block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
+        )
+
+        # CloudFront Distribution
+        distribution = cloudfront.Distribution(
+            self,
+            "FrontendDistribution",
+            default_root_object="index.html",
+            default_behavior=cloudfront.BehaviorOptions(
+                origin=origins.S3Origin(bucket), viewer_protocol_policy=cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS
+            ),
+        )
+
+        # Deploy files to S3
+        s3deploy.BucketDeployment(
+            self,
+            "DeployFrontend",
+            sources=[s3deploy.Source.asset("../frontend/dist")],  # Adjust path if needed
+            destination_bucket=bucket,
+            distribution=distribution,
+            distribution_paths=["/*"],  # Invalidate CloudFront cache
+        )
+
+        # Output the CloudFront URL
+        CfnOutput(self, "CloudFrontURL", value=distribution.distribution_domain_name, export_name="MyFrontendURL")
