368feacc16dcbab640318a925776940e0a40a37c
Antoine Delia <antoine.delia@gmail.com>
2025-06-01 17:05:26 +0200

    feat: add route53

--- iac-cdk/app.py
+++ iac-cdk/app.py
@@ -7,6 +7,12 @@ from frontend.frontend_stack import FrontendStack
 app = cdk.App()
 
 BackendStack(app, "BackendStack", env=cdk.Environment(account="646082475080", region="eu-west-1"))
-FrontendStack(app, "FrontendStack", env=cdk.Environment(account="646082475080", region="eu-west-1"))
+FrontendStack(
+    app,
+    "FrontendStack",
+    domain_name="antoinedelia.fr",
+    subdomain="spotify-cover",
+    env=cdk.Environment(account="646082475080", region="eu-west-1"),
+)
 
 app.synth()
--- iac-cdk/frontend/frontend_stack.py
+++ iac-cdk/frontend/frontend_stack.py
@@ -1,22 +1,27 @@
 from aws_cdk import CfnOutput, RemovalPolicy, Stack
-from aws_cdk import (
-    aws_cloudfront as cloudfront,
-)
-from aws_cdk import (
-    aws_cloudfront_origins as origins,
-)
-from aws_cdk import (
-    aws_s3 as s3,
-)
-from aws_cdk import (
-    aws_s3_deployment as s3deploy,
-)
+from aws_cdk import aws_certificatemanager as acm
+from aws_cdk import aws_cloudfront as cloudfront
+from aws_cdk import aws_cloudfront_origins as origins
+from aws_cdk import aws_route53 as route53
+from aws_cdk import aws_route53_targets as targets
+from aws_cdk import aws_s3 as s3
+from aws_cdk import aws_s3_deployment as s3deploy
 from constructs import Construct
 
 
 class FrontendStack(Stack):
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: Construct, construct_id: str, domain_name: str, subdomain: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+
+        full_domain = f"{subdomain}.{domain_name}"
+
+        # Get the existing hosted zone
+        zone = route53.HostedZone.from_lookup(self, "HostedZone", domain_name=domain_name)
+
+        # Certificate in us-east-1 (required for CloudFront)
+        certificate = acm.DnsValidatedCertificate(
+            self, "SiteCertificate", domain_name=full_domain, hosted_zone=zone, region="us-east-1"
+        )
 
         # S3 Bucket (private)
         bucket = s3.Bucket(
@@ -34,6 +39,7 @@ class FrontendStack(Stack):
             self,
             "FrontendDistribution",
             default_root_object="index.html",
+            certificate=certificate,
             default_behavior=cloudfront.BehaviorOptions(
                 origin=origins.S3BucketOrigin(bucket), viewer_protocol_policy=cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS
             ),
@@ -47,6 +53,15 @@ class FrontendStack(Stack):
             destination_bucket=bucket,
             distribution=distribution,
             distribution_paths=["/*"],  # Invalidate CloudFront cache
+        )
+
+        # Route53 Alias A Record
+        route53.ARecord(
+            self,
+            "FrontendAliasRecord",
+            zone=zone,
+            record_name=subdomain,
+            target=route53.RecordTarget.from_alias(targets.CloudFrontTarget(distribution)),
         )
 
         # Output the CloudFront URL
